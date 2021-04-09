d985608a49b4928c97e50e6d87bf1bf5748271ee
Nicolas Klein <ncl.klein@gmail.com>
2021-04-09 15:41:00 +0200

    More logging of rEST Thumb service

--- rest/thumber.go
+++ rest/thumber.go
@@ -23,26 +23,31 @@ func (t *ThumberAPI) InitRoutes(r *mux.Router) {
 }
 
 func (t *ThumberAPI) createThumb(w http.ResponseWriter, r *http.Request) {
+	log, _ := logging.SubFrom(r.Context(), "thumber")
 	f := mux.Vars(r)["format"]
 	format, found := domain.FormatForExt(f)
 	if !found {
+		log.Warn("Missing input format param")
 		Respond(r).WithError(w, http.StatusBadRequest, fmt.Errorf("No such format '%s'", f))
 		return
 	}
 	s := mux.Vars(r)["size"]
 	size, found := domain.ThumbSizes[s]
 	if !found {
+		log.Warn("Invalid thumb size", zap.String("size", s))
 		Respond(r).WithError(w, http.StatusBadRequest, fmt.Errorf("Invalid thumb size '%s'", s))
 		return
 	}
 	contentType := r.Header.Get("Content-Type")
 	inputFormat, found := domain.FormatForMime(contentType)
 	if !found {
+		log.Warn("Unknown input format", zap.String("format", f))
 		Respond(r).WithError(w, http.StatusNotImplemented, fmt.Errorf("Input format '%s' not suported", contentType))
 		return
 	}
 	thumb, err := t.thumber.CreateThumb(r.Body, inputFormat, domain.NormalOrientation, size)
 	if err != nil {
+		log.Warn("Failed to create thumb", zap.String("format", f), zap.Error(err))
 		Respond(r).WithError(w, http.StatusInternalServerError, err)
 		return
 	}
